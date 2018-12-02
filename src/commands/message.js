78642a5eb12b9a7b5f642c5c632fc566c7545eb7
Erdemhso <41632465+erdemsweb@users.noreply.github.com>
2018-12-02 18:45:49 +0200

    Update message.js

--- src/commands/message.js
+++ src/commands/message.js
@@ -216,8 +216,7 @@ class CommandMessage {
 				const invite = this.client.options.invite;
 				return this.reply(stripIndents`
 					Komutu kullanırken bir hata oluştu!: \`${err.name}: ${err.message}\`
-					Lütfen bildirin: https://discord.gg/GvfuXmE
-				`);
+					Hatayı lütfen bot sahiplerine bildirin.`);
 			}
 		}
 	}
